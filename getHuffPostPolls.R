c057d230a464e47b8a0ef3e662e604b6a0884118
gjm112 <gjm112@gmail.com>
2016-08-20 15:04:57 -0500

    Update getHuffPostPolls.R

--- getHuffPostPolls.R
+++ getHuffPostPolls.R
@@ -101,5 +101,90 @@ polygon(c(pDat14$date,pDat14$date[53:1]),c(pDat14$trump-100*pDat14$moeT,pDat14$t
 polygon(c(pDat21$date,pDat21$date[53:1]),c(pDat21$clinton-100*pDat21$moeC,pDat21$clinton[53:1]+100*pDat21$moeC[53:1]),col=rgb(0,0,1,0.5),border=rgb(0,0,1,0.1))
 polygon(c(pDat21$date,pDat21$date[53:1]),c(pDat21$trump-100*pDat21$moeT,pDat21$trump[53:1]+100*pDat21$moeT[53:1]),col=rgb(1,0,0,0.5),border=rgb(1,0,0,0.1))
 
+dev.off()
+
+
+
+#State polls
+stateIndex<-grep("16-[A-z]{2}-Pres-GE TrumpvClinton",unlist(codes))
+stateIndex<-setdiff(stateIndex,usIndex)
+
+
+pollList<-list()
+for (i in stateIndex){print(i)
+  n<-datList[[i]]$questions[[1]]$subpopulations[[1]]$observations
+  if (is.null(n)){n<-NA}
+  
+  state<-substring(datList[[i]]$questions[[1]]$code[[1]],4,5)
+  
+  jjj<-length(datList[[i]]$questions[[1]]$subpopulations[[1]]$responses)
+  tempList<-list()
+  for (j in 1:jjj){
+    tempList[[datList[[i]]$questions[[1]]$subpopulations[[1]]$responses[[j]]$choice]]<-datList[[i]]$questions[[1]]$subpopulations[[1]]$responses[[j]]$value
+  }
+  
+  if ("Clinton"%in%names(tempList) & "Trump"%in%names(tempList)){
+    
+    pollList[[i]]<-
+      data.frame(pollster = datList[[i]]$pollster,
+                 state = state,
+                 start = datList[[i]]$start_date,
+                 end = datList[[i]]$end_date,
+                 method = datList[[i]]$method,
+                 part = datList[[i]]$partisan,
+                 who = datList[[i]]$questions[[1]]$subpopulations[[1]]$name,
+                 n = n,
+                 clinton = unlist(tempList)[grep("Clinton",names(unlist(tempList)))],
+                 trump = unlist(tempList)[grep("Trump",names(unlist(tempList)))])
+    
+  }
+  
+}
+
+polls<-do.call(rbind,pollList)
+polls<-subset(polls,who%in%c("Registered Voters","Likely Voters"))
+polls<-polls[polls$state!="US",]
+polls$end<-as.Date(polls$end)
+
+pList<-list()
+states<-sort(names(table(polls$state)))
+states <- setdiff(states,"US")
+
+png("/Users/gregorymatthews/Dropbox/StatsInTheWild/TrumpVsClintonStatePolls.png",h=10,w=10,units="in",res=300)
+plot(c(0.2,0.6),c(0.2,.55),col="white",xlab="Clinton %",ylab="Trump %",main="State polls",sub="@statsinthewild")
+polygon(c(0,1,1),c(0,0,1),col=rgb(0,0,1,0.2))
+polygon(c(0,0,1),c(0,1,1),col=rgb(1,0,0,0.2))
+abline(a=0,b=1)
+abline(a=1,b=-1)
+abline(a=0.75,b=-1)
+abline(a=0.5,b=-1)
+for (st in states){
+temp<-polls[polls$state==st,]
+pClinton<-sum(temp$clinton/100*temp$n)/sum(temp$n)
+sdC<-sqrt(pClinton*(1-pClinton)/sum(temp$n))
+pTrump<-sum(temp$trump/100*temp$n)/sum(temp$n)
+sdT<-sqrt(pTrump*(1-pTrump)/sum(temp$n))
+
+ccc<-rnorm(5000,pClinton,sdC)
+ttt<-rnorm(5000,pTrump,sdT)
+points(ccc,ttt,pch=16,cex=0.5,col=rgb(2*(pTrump-0.2),0,2*(pClinton-0.2),0.1))
+}
+abline(a=0,b=1)
+abline(a=1,b=-1)
+abline(a=0.75,b=-1)
+abline(a=0.5,b=-1)
+
+for (st in states){
+  temp<-polls[polls$state==st,]
+  pClinton<-sum(temp$clinton/100*temp$n)/sum(temp$n)
+  sdC<-sqrt(pClinton*(1-pClinton)/sum(temp$n))
+  pTrump<-sum(temp$trump/100*temp$n)/sum(temp$n)
+  sdT<-sqrt(pTrump*(1-pTrump)/sum(temp$n))
+  
+
+  text(pClinton,pTrump,st,col=rgb(0.9,0.9,0.9,0.9),cex=10*abs(pClinton-pTrump)+0.5)
+}
 
 dev.off()
+
+
