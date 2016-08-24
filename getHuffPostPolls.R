d999f649b7b9d0fc592bdf22ed12d7cc0c5fbe6f
gjm112 <gjm112@gmail.com>
2016-08-24 11:57:50 -0500

    Update getHuffPostPolls.R

--- getHuffPostPolls.R
+++ getHuffPostPolls.R
@@ -7,7 +7,7 @@ for (i in 1:10){
   datList[[i]]<-x[[i]]
 }
 
-for (n in 1001:10000){print(n)
+for (n in 2:700){print(n)
 x<-fromJSON(getURL(paste0("http://elections.huffingtonpost.com/pollster/api/polls.json?page=",n)))
 for (i in 1:10){
 datList[[10*(n-1)+i]]<-x[[i]]
@@ -19,10 +19,10 @@ getCode<-function(x){
 datList[[x]]$questions[[1]]$code
 }
 
-codes<-lapply(c(1:10000),getCode)
+codes<-lapply(c(1:500),getCode)
 
 usIndex<-grep("16-US-Pres-GE TrumpvClinton",unlist(codes))
-usIndex<-setdiff(usIndex, c(608,619,630,645,661,666,670))
+#usIndex<-setdiff(usIndex, c(608,619,630,645,661,666,670))
 
 pollList<-list()
 for (i in usIndex){print(i)
@@ -67,7 +67,7 @@ temp<-polls[polls$end<=dates[i] & polls$end>=dates[i]-14,]
 pList[[i]]<-data.frame(date=dates[i],clinton= mean(temp$clinton,weight=temp$n),trump = mean(temp$trump,weight=temp$n))
 }
 
-png("/Users/gregorymatthews/Dropbox/StatsInTheWild/TrumpVsClintonNationalPolls.png",h=10,w=20,units="in",res=300)
+#png("/Users/gregorymatthews/Dropbox/StatsInTheWild/TrumpVsClintonNationalPolls.png",h=10,w=20,units="in",res=300)
 pDat<-do.call(rbind,pList)
 plot(pDat$date,pDat$clinton,type="l",ylim=c(33,51),col="white",lwd=3,xlab="Date",ylab="%",main="Trump vs Clinton Polls\n Weighted Moving Average\n Source: Huffington Post",sub="@statsinthewild")
 points(pDat$date,pDat$trump,type="l",ylim=c(33,51),col="white",lwd=3)
@@ -92,16 +92,16 @@ points(pDat$date,pDat$clinton,type="l",ylim=c(0,100),col=rgb(0,0,lw/21,lw/30),lw
 points(pDat$date,pDat$trump,type="l",ylim=c(0,100),col=rgb(lw/21,0,0,lw/30),lwd=lw/5)
 }
 
-polygon(c(pDat7$date,pDat7$date[53:1]),c(pDat7$clinton-100*pDat7$moeC,pDat7$clinton[53:1]+100*pDat7$moeC[53:1]),col=rgb(0,0,1,0.5),border=rgb(0,0,1,0.1))
-polygon(c(pDat7$date,pDat7$date[53:1]),c(pDat7$trump-100*pDat7$moeT,pDat7$trump[53:1]+100*pDat7$moeT[53:1]),col=rgb(1,0,0,0.5),border=rgb(1,0,0,0.1))
+polygon(c(pDat7$date,pDat7$date[nrow(pDat7):1]),c(pDat7$clinton-100*pDat7$moeC,pDat7$clinton[nrow(pDat7):1]+100*pDat7$moeC[nrow(pDat7):1]),col=rgb(0,0,1,0.5),border=rgb(0,0,1,0.1))
+polygon(c(pDat7$date,pDat7$date[nrow(pDat7):1]),c(pDat7$trump-100*pDat7$moeT,pDat7$trump[nrow(pDat7):1]+100*pDat7$moeT[nrow(pDat7):1]),col=rgb(1,0,0,0.5),border=rgb(1,0,0,0.1))
 
-polygon(c(pDat14$date,pDat14$date[53:1]),c(pDat14$clinton-100*pDat14$moeC,pDat14$clinton[53:1]+100*pDat14$moeC[53:1]),col=rgb(0,0,1,0.5),border=rgb(0,0,1,0.1))
-polygon(c(pDat14$date,pDat14$date[53:1]),c(pDat14$trump-100*pDat14$moeT,pDat14$trump[53:1]+100*pDat14$moeT[53:1]),col=rgb(1,0,0,0.5),border=rgb(1,0,0,0.1))
+polygon(c(pDat14$date,pDat14$date[nrow(pDat14):1]),c(pDat14$clinton-100*pDat14$moeC,pDat14$clinton[nrow(pDat14):1]+100*pDat14$moeC[nrow(pDat14):1]),col=rgb(0,0,1,0.5),border=rgb(0,0,1,0.1))
+polygon(c(pDat14$date,pDat14$date[nrow(pDat14):1]),c(pDat14$trump-100*pDat14$moeT,pDat14$trump[nrow(pDat14):1]+100*pDat14$moeT[nrow(pDat14):1]),col=rgb(1,0,0,0.5),border=rgb(1,0,0,0.1))
 
-polygon(c(pDat21$date,pDat21$date[53:1]),c(pDat21$clinton-100*pDat21$moeC,pDat21$clinton[53:1]+100*pDat21$moeC[53:1]),col=rgb(0,0,1,0.5),border=rgb(0,0,1,0.1))
-polygon(c(pDat21$date,pDat21$date[53:1]),c(pDat21$trump-100*pDat21$moeT,pDat21$trump[53:1]+100*pDat21$moeT[53:1]),col=rgb(1,0,0,0.5),border=rgb(1,0,0,0.1))
+polygon(c(pDat21$date,pDat21$date[nrow(pDat21):1]),c(pDat21$clinton-100*pDat21$moeC,pDat21$clinton[nrow(pDat21):1]+100*pDat21$moeC[nrow(pDat21):1]),col=rgb(0,0,1,0.5),border=rgb(0,0,1,0.1))
+polygon(c(pDat21$date,pDat21$date[nrow(pDat21):1]),c(pDat21$trump-100*pDat21$moeT,pDat21$trump[nrow(pDat21):1]+100*pDat21$moeT[nrow(pDat21):1]),col=rgb(1,0,0,0.5),border=rgb(1,0,0,0.1))
 
-dev.off()
+#dev.off()
 
 
 
@@ -150,7 +150,7 @@ pList<-list()
 states<-sort(names(table(polls$state)))
 states <- setdiff(states,"US")
 
-png("/Users/gregorymatthews/Dropbox/StatsInTheWild/TrumpVsClintonStatePolls.png",h=10,w=10,units="in",res=300)
+png("/Users/gregorymatthews/Dropbox/StatsInTheWild/TrumpVsClintonStatePolls.png",h=10,w=10,units="in",res=100)
 plot(c(0.2,0.6),c(0.2,.55),col="white",xlab="Clinton %",ylab="Trump %",main="State polls",sub="@statsinthewild")
 polygon(c(0,1,1),c(0,0,1),col=rgb(0,0,1,0.2))
 polygon(c(0,0,1),c(0,1,1),col=rgb(1,0,0,0.2))
